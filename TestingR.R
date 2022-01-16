2b0b736ffacf72b69e3e248792e53930e60d8133
Matheus Pizzani <matheus_pizzani@hotmail.com>
2022-01-16 16:45:50 -0300

    Second Commit with R

--- TestingR.R
+++ TestingR.R
@@ -1,3 +1,7 @@
 print ("This File was created within R Studio")
 
 print ("And now it lives on Github")
+
+
+use_git_config(user.name = "Matheus Pizzani", # Seu nome
+               +                user.email = "matheus_pizzani@hotmail.com") # Seu email
